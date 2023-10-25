59219f0f81121b26d557140f330a41af216bcfc8
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-10-25 09:17:48 -0400

    Update TestIfOnline.psm1
    added disclaimer

--- TestIfOnline/TestIfOnline.psm1
+++ TestIfOnline/TestIfOnline.psm1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    This script checks the online status of a specified computer.
+
+.DESCRIPTION
+    The script continuously checks the connectivity status of the specified computer using the Test-NetConnection cmdlet. It provides real-time feedback regarding the online or offline status of the target computer.
+
+.NOTES
+    Author: Alex McPadden
+    Disclaimer: This script is provided as-is without any warranty. The author is not responsible for any potential damages or issues caused by the use of this script. Users are advised to test the script in non-production environments before using it in a production setting.
+
+.LINK
+    GitHub: https://github.com/alexmczz/TestIfOnline
+
+#>
+
 function TestIfOnline {
     [CmdletBinding()]
     param (
