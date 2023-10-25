fe4029082dcdd5b555ac19374e9c7bbe8e3de8e4
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-10-25 08:51:14 -0400

    Add files via upload

---
+++ TestIfOnline/TestIfOnline.psm1
@@ -0,0 +1,20 @@
+﻿
+
+function TestIfOnline {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$pcName
+    )
+
+    while($true) {
+        $testPconline = Test-NetConnection -ComputerName $pcName -InformationLevel Detailed
+        if ($testPconline.PingSucceeded) {
+            Write-Host "Online"
+            [console]::Beep(500,500)
+        } else {
+            Write-Host "Offline"
+        }
+        Start-Sleep -Seconds 5  # Adjust this value as needed for the delay between each check
+    }
+}
